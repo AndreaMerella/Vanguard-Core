2e18a85213bb3e550ec6d535763a170fe6095189
AndreaMerella <71179576+AndreaMerella@users.noreply.github.com>
2026-02-12 09:17:48 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
-const CACHE_NAME = 'vngrd-v1';
-const assets = ['./indexbackup.html'];
+const CACHE_NAME = 'vngrd-v2'; // Incrementing version clears the old laggy cache
+const assets = [
+  './index.html',
+  './src/app.js',
+  './src/Compositor.js'
+];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces the new, faster worker to take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+    // This strategy serves from cache first, then network, to stop the lag
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
